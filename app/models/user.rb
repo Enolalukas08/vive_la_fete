57a08b559cdaf72e559b232ba1bb7d8e5b6f3295
enola lukasiewicz <enola.lukasiewicz@kedgebs.com>
2023-08-21 16:41:32 +0200

    devise add

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, format: { with: /\A.*@.*\.com\z/ }, presence: true
 end
