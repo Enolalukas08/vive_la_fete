2282e3f24199d76c71d0fa364ebe7cced0192a92
enola lukasiewicz <enola.lukasiewicz@kedgebs.com>
2023-08-21 17:03:12 +0200

    controllers

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @articles
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:name, :description, :price)
+  end
+end
