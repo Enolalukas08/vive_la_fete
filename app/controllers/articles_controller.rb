184855abc7fe3199b9607fd27284ca59f9b1263e
enola lukasiewicz <enola.lukasiewicz@kedgebs.com>
2023-08-22 15:53:22 +0200

    navbar et footer

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -46,6 +46,6 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:name, :description, :price)
+    params.require(:article).permit(:name, :description, :price, :photo)
   end
 end
