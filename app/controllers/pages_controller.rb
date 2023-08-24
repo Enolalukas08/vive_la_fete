47024a0bfd55f779241d68cd46af143f474ebf6d
enola lukasiewicz <enola.lukasiewicz@kedgebs.com>
2023-08-24 18:14:42 +0200

    added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def my_bookings
+    @my_bookings = Booking.where(user: current_user)
+  end
+
+  def my_articles
+    @my_articles = Article.where(user: current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :bookings, only: %i[new create]
   end
   resources :bookings, only: %i[index show]
+  get 'my_bookings', to: 'pages#my_bookings'
+  get 'my_articles', to: 'pages#my_articles'
 end
 
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
