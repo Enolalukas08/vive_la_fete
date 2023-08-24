7706e01572c82b7776cff6bdeebe1de0af0d5276
Alexandre Hary <alexandre.harry10@gmail.com>
2023-08-24 12:01:57 +0200

    the map works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_130540) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_124448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,13 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_130540) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "addresses", force: :cascade do |t|
-    t.string "street"
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "articles", force: :cascade do |t|
